a35b18aaddfa975d797bdd4746527533cbf81600
horhik <horhik@tuta.io>
2020-02-02 20:35:15 +0300

    add basic comunication with server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    server: "http://localhost:5000"
-}
+  server: 'http://localhost:5000',
+  //  server: '192.168.0.105:5000',
+  host: 'localhost:5000',
+};
 
-export default config
+export default config;
